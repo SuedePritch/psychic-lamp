8381ceaf2e8e81b83aa65dc930e8bb50a5c3d757
James Pritchard <james.edward.pritchard@gmail.com>
2022-05-18 23:32:55 -0400

    added character strings and concatenated from selected characters

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //Global Variables
+var lowercase = 'abcdefghijklmnopqrstuvwxyz'
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var numbers = '1234567890'
+var special = '!@#$%^&*()_+{}|:"<>?-=[];,./'
 
 var numOfChar = 0;
 //Default to strongest password
@@ -31,6 +35,45 @@ function createPassword() {
   let useUpper = confirm('Do you want uppercase letters');
   let useNumbers = confirm('Do you want to use numbers');
   let useSpecial = confirm('Do you want to use special characters')
+  passwordStrength();
+  
+  
+  
+  
+  function passwordStrength(){
+    //no selection made
+    if(!useLower && !useUpper && !useNumbers && !useSpecial){
+      alert('You want your password to be made of... hopes and dreams???  You gotta choose something buddy!');
+      createPassword();
+      //lowercase and
+    }else if(useLower && !useUpper && !useNumbers && !useSpecial){
+      passwordStrength = lowercase;
+    }else if(useLower && useUpper && !useNumbers && !useSpecial){
+      passwordStrength = lowercase.concat(uppercase);
+    }else if(useLower && useUpper && useNumbers && !useSpecial){
+      passwordStrength = lowercase.concat(uppercase, numbers);
+    }else if(useLower && useUpper && useNumbers && useSpecial){
+      passwordStrength = lowercase.concat(uppercase, numbers, special);
+      //no lowercase
+    }else if(!useLower && useUpper && !useNumbers && !useSpecial){
+      passwordStrength = uppercase;
+    }else if(!useLower && useUpper && useNumbers && !useSpecial){
+      passwordStrength = uppercase.concat(numbers);
+    }else if(!useLower && useUpper && useNumbers && useSpecial){
+      passwordStrength = uppercase.concat(numbers, special);
+      //no letters
+    }else if(!useLower && !useUpper && useNumbers && !useSpecial){
+      passwordStrength = numbers;
+    }else if(!useLower && !useUpper && useNumbers && useSpecial){
+      passwordStrength = numbers.concat(special);
+      //only special
+    }else if(!useLower && !useUpper && !useNumbers && useSpecial){
+      passwordStrength = special;
+    }else{
+      alert('something went wrong')
+    }
+    console.log(passwordStrength);
+  }
   passwordText.value = password;
   
 }
