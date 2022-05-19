202e2bd5a42ee15d9af6e044c379bf5fc1055a19
James Pritchard <james.edward.pritchard@gmail.com>
2022-05-18 23:13:07 -0400

    changed writePassword name to createPassword

--- script.js
+++ script.js
@@ -8,31 +8,32 @@ var useNumbers = true;
 var useSpecial = true;
 
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
-function writePassword() {
+function createPassword() {
   // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
   
   
   //Prompts
   numOfChar = prompt('Password length')
+  
+  //Error handling for the prompt numOfCharacters between 8-128
+  if(numOfChar < 7){
+    alert('Needs to be at least 8 characters')
+    createPassword();
+  }else if(numOfChar > 127){
+    alert('Thats too long: max 128');
+    createPassword();
+  }
+
   let useLower = confirm('Do you want to use lowercase letters')
   let useUpper = confirm('Do you want uppercase letters');
   let useNumbers = confirm('Do you want to use numbers');
   let useSpecial = confirm('Do you want to use special characters')
-
-  //Error handling for the prompt numOfCharacters
-  if(numOfChar < 7){
-    alert('Needs to be at least 8 characters')
-  }else if(numOfChar > 127){
-    alert('Thats too long')
-  }else{ 
-    console.log(numOfChar);
-  }
-  // passwordText.value = password;
+  passwordText.value = password;
   
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", createPassword);
